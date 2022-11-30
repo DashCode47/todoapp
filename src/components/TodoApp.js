388664e1ac060af9321e1e8dd2cc5086e4676a03
David Lozada <davsh47@hotmail.com>
2022-11-30 23:01:11 +0300

    todo-list
    finished

---
+++ src/components/TodoApp.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import Todo from "./Todo";
+import "./todoApp.css";
+const TodoApp = () => {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setTitle("David");
+  };
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setTitle(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false,
+    };
+    const temp = [...todos];
+    temp.unshift(newTodo);
+    setTodos(temp);
+    setTitle("");
+  };
+
+  const handleUpdate = (id, value) => {
+    const temp = [...todos];
+    const item = temp.find((item) => item.id === id);
+    item.title = value;
+    setTodos(temp);
+  };
+
+  const handleDelete = (id) => {
+    const temp = todos.filter((item) => item.id !== id);
+    setTodos(temp);
+  };
+  return (
+    <div className="todoContainer">
+      <img src={require("../assets/logo.png")} className="kruger" />
+      <div className="titulo">TO-DO-APP</div>
+      <form className="todoCreateForm" onSubmit={handleSubmit}>
+        <input onChange={handleChange} className="todoInput" value={title} />
+        <input
+          onClick={handleSubmit}
+          type={"submit"}
+          value="Create todo"
+          className="buttonCreate"
+        />
+      </form>
+
+      <div className="todosContainer">
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            item={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoApp;
